587155aaa6a4506b0471d54d15ec3abcec0cc1da
MAHRifat <hasanrifata2@gmail.com>
2024-09-28 23:33:04 +0600

    learn object3D() and its 3 properties and group and helpers

--- Three.Js Refresher/Hello cube/index.js
+++ Three.Js Refresher/Hello cube/index.js
@@ -7,12 +7,38 @@ console.log(THREE);
 
 const scene = new THREE.Scene();
 
+// Group
+const group = new THREE.Group();
+
 // Mesh
 const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: "white"});
+const material = new THREE.MeshBasicMaterial({color: "red"});
 const mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = 1;
+// mesh.position.y = 1;
+// mesh.position.z = 1;
+mesh.scale.x = 2;
+mesh.rotation.x = Math.PI * 1.57;
+mesh.rotation.y = Math.PI * 1.2;
+// scene.add(mesh);
 
-scene.add(mesh);
+// Mesh 2
+
+const geometryT = new THREE.BoxGeometry(1,1,1);
+const materialT = new THREE.MeshBasicMaterial({color: "green"});
+
+const meshT = new THREE.Mesh(geometryT, materialT);
+// scene.add(meshT);
+
+//group
+
+group.add(mesh, meshT);
+group.position.x = 3;
+scene.add(group);
+
+// AxesHelper
+const axesHelper = new THREE.AxesHelper( 2 );
+scene.add( axesHelper );
 
 // Camera
 
@@ -21,9 +47,9 @@ const aspect = {
     height: window.innerHeight
 };
 const camera = new THREE.PerspectiveCamera(75,aspect.width/aspect.height);   // near 1 and far 2000 is the default value
-camera.position.z = 3;
-camera.position.x =1;
+camera.position.x = 1;
 camera.position.y = 1;
+camera.position.z = 3;
 scene.add(camera);
 
 
