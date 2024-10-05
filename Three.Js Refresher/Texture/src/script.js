d7c99e1c1ae8efe207cdea377ded81d210018378
MAHRifat <hasanrifata2@gmail.com>
2024-10-05 22:47:23 +0600

    Texture lecture complete

---
+++ Three.Js Refresher/Texture/src/script.js
@@ -0,0 +1,87 @@
+import "./style.css";
+import * as THREE from "three";
+import {
+  MapControls,
+  OrbitControls,
+} from "three/examples/jsm/controls/OrbitControls";
+
+//Scene
+const scene = new THREE.Scene();
+
+// loadingManager -> use loading manager for texture when we upload the website it will reload first
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onStart = ()=>{
+  console.log("start");
+};
+loadingManager.onLoad = ()=>{
+  console.log("loading...");
+};
+loadingManager.onProgress = ()=>{
+  console.log("progress");
+};
+loadingManager.onError = ()=>{
+  console.log("Error!");
+};
+
+// textureLoader
+const texture = new THREE.TextureLoader(loadingManager);
+const colorTexture = texture.load("/texture/color.jpg");
+console.log(texture);
+
+//Resizing
+window.addEventListener("resize", () => {
+  //Update Size
+  aspect.width = window.innerWidth;
+  aspect.height = window.innerHeight;
+
+  //New Aspect Ratio
+  camera.aspect = aspect.width / aspect.height;
+  camera.updateProjectionMatrix();
+
+  //New RendererSize
+  renderer.setSize(aspect.width, aspect.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+//Mesh
+const geometry = new THREE.PlaneBufferGeometry(1, 1);
+console.log(geometry);
+const material = new THREE.MeshBasicMaterial({ map: colorTexture });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//Camera
+const aspect = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
+camera.position.z = 1;
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector(".draw");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(aspect.width, aspect.height);
+
+//OrbitControls
+const orbitControls = new OrbitControls(camera, canvas);
+orbitControls.enableDamping = true;
+
+//Clock Class
+const clock = new THREE.Clock();
+
+const animate = () => {
+  //GetElapsedTime
+  const elapsedTime = clock.getElapsedTime();
+
+  //Update Controls
+  orbitControls.update();
+
+  //Renderer
+  renderer.render(scene, camera);
+
+  //RequestAnimationFrame
+  window.requestAnimationFrame(animate);
+};
+animate();
